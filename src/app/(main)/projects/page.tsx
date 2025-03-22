260bc17c47d16f840b907d0e12499cc9cb6dd99a
sheshh00 <149348156+sheshh00@users.noreply.github.com>
2025-03-23 01:07:24 +0800

    clear page with title

--- src/app/(main)/projects/page.tsx
+++ src/app/(main)/projects/page.tsx
@@ -1,3 +1,11 @@
 export default async function ProjectListPage() {
-    return <div>Project List</div>;
+    return (
+        <div className="bg-white-50 min-h-screen">
+            <div className="mr-10 p-8 font-sans text-xl font-bold">
+                Concrete Works Testing RMS
+            </div>
+        </div>
+    );
+
+    // return <div>Project List</div>;
 }
