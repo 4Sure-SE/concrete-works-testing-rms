db1143a2f8155569e488c8c186ffd2bcbba92832
Jose <joseearronn@gmail.com>
2025-03-23 12:44:33 +0800

    feat: add app header and sidebar

---
+++ src/app/(main)/_components/app-sidebar/index.ts
@@ -0,0 +1,5 @@
+import AppSidebar from "./app-sidebar";
+import NavProjects from "./nav-projects";
+import NavProjectsSkeleton from "./nav-projects-skeleton";
+
+export { AppSidebar, NavProjects, NavProjectsSkeleton };
--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,5 +1,41 @@
+import "@/styles/globals.css";
+
+import Header from "@/components/custom/header";
+import { SidebarProvider } from "@/components/ui/sidebar";
+import { type Metadata } from "next";
+import { Inter } from "next/font/google";
+import { AppSidebar } from "./_components/app-sidebar";
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+
+export const metadata: Metadata = {
+    title: "Concrete Works Testing RMS",
+    description: "Concrete Works Testing RMS",
+    icons: [{ rel: "icon", url: "/favicon.ico" }],
+};
+
 export default function MainLayout({
     children,
 }: Readonly<{ children: React.ReactNode }>) {
-    return <div>{children}</div>;
+    return (
+        <html
+            lang="en"
+            className={`${inter.className} ${inter.variable}`}
+        >
+            <body>
+                <SidebarProvider>
+                    <AppSidebar />
+                    <div className="w-full">
+                        <Header
+                            withSidebarTrigger
+                            title="Concrete Works Testing RMS"
+                        />
+                        <main className="flex-1 overflow-auto p-4">
+                            {children}
+                        </main>
+                    </div>
+                </SidebarProvider>
+            </body>
+        </html>
+    );
 }
