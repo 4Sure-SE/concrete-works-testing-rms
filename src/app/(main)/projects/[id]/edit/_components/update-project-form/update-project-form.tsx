a8243cf274599ac9a0cb956abf2ee812a297a07d
okjose <joseearronn@gmail.com>
2025-04-27 19:33:10 +0800

    feat: implement edit project with form and server-side validation (#26)

---
+++ src/app/(main)/projects/[id]/edit/_components/update-project-form/update-project-form.tsx
@@ -0,0 +1,121 @@
+"use client";
+
+import Form from "next/form";
+import { useRouter } from "next/navigation";
+
+import FormRow from "@/components/custom/form-row";
+import { Form as UIForm } from "@/components/ui/form";
+import { useFormAction } from "@/hooks/use-form-action";
+import { type Callbacks } from "@/lib/types/actions.types";
+import type {
+    ProjectActionErrors,
+    ProjectActionState,
+    ProjectDTO,
+    UpdateProjectDTO,
+} from "@/lib/types/project/project.types";
+import { withCallbacks } from "@/lib/utils";
+
+import { ProjectFormField } from "@/app/(main)/projects/_components/project-form";
+import { formLayout } from "@/app/(main)/projects/_components/project-form/project-form.config";
+import ButtonWithLoader from "@/components/custom/button-with-loader";
+import { updateProjectFormConfig } from "./update-project-form.config";
+import { updateProjectSchema } from "./update-project-form.schema";
+
+interface UpdateProjectFormProps {
+    projectId: string;
+    action: (
+        projectId: string,
+        previousState: ProjectActionState,
+        formData: FormData,
+    ) => Promise<ProjectActionState>;
+    defaultValues: UpdateProjectDTO;
+}
+
+export default function UpdateProjectForm({
+    projectId,
+    action,
+    defaultValues,
+}: UpdateProjectFormProps) {
+    const router = useRouter();
+
+    // server action callbacks on success or error
+    const callbacks: Callbacks<ProjectDTO | null, ProjectActionErrors> = {
+        // on server action success
+        onSuccess: (_data) => {
+            router.back();
+            form.reset();
+        },
+        // on server action error
+        onError: (error) => {
+            if (error.general)
+                form.setError("root", { message: error.general[0] });
+            if (error.contractId)
+                form.setError("contractId", { message: error.contractId[0] });
+        },
+    };
+
+    const { form, isPending, startAction, submitAction } = useFormAction({
+        action: withCallbacks(action.bind(null, projectId), callbacks),
+        schema: updateProjectSchema,
+        defaultValues,
+    });
+
+    // handle form submission and call the server action with the form data
+    const handleSubmit = form.handleSubmit((_, e) => {
+        startAction(() => {
+            const formData = new FormData(e?.target as HTMLFormElement);
+            submitAction(formData);
+        });
+    });
+
+    return (
+        // shadcn form component to handle form state and validation
+        <UIForm {...form}>
+            {/* next/form to handle form submission */}
+            <Form
+                action={submitAction}
+                onSubmit={handleSubmit}
+                noValidate
+                className="space-y-8"
+            >
+                {/* form fields */}
+                <div className="space-y-6">
+                    {/* render each row based on the layout in config */}
+                    {formLayout.map((row, rowIndex) => (
+                        <FormRow
+                            className="gap-4 md:gap-10"
+                            key={`row-${rowIndex}`}
+                        >
+                            {/* render each field on the row*/}
+                            {row.map((fieldName) => (
+                                <ProjectFormField<UpdateProjectDTO>
+                                    key={fieldName}
+                                    control={form.control}
+                                    fieldDetails={
+                                        updateProjectFormConfig[fieldName]
+                                    }
+                                />
+                            ))}
+                        </FormRow>
+                    ))}
+                </div>
+
+                {/* display general server error */}
+                {form.formState.errors.root ? (
+                    <span className="font-medium text-destructive">
+                        {form.formState.errors.root.message}
+                    </span>
+                ) : null}
+
+                {/* submit button */}
+                <div className="flex justify-end">
+                    <ButtonWithLoader
+                        isPending={isPending}
+                        text="Update"
+                        type="submit"
+                    />
+                </div>
+            </Form>
+        </UIForm>
+    );
+}
--- src/app/(main)/projects/[id]/edit/page.tsx
+++ src/app/(main)/projects/[id]/edit/page.tsx
@@ -1,8 +1,34 @@
+import SectionHeader from "@/components/custom/section-header";
+import { tryCatch } from "@/lib/utils";
+import { updateProject } from "@/server/actions/projects/update-project";
+import { ProjectService } from "@/server/services/project.service";
+import UpdateProjectForm from "./_components/update-project-form/update-project-form";
+
 export default async function ProjectEditPage({
     params,
 }: {
     params: Promise<{ id: string }>;
 }) {
     const { id } = await params;
-    return <div>Edit Project: {id}</div>;
+    const { data: project, error: getProjectError } = await tryCatch(
+        ProjectService.getProjectById(id),
+    );
+
+    if (getProjectError) {
+        throw getProjectError;
+    }
+
+    return (
+        <div className="mx-auto max-w-7xl py-2 md:py-4">
+            <SectionHeader
+                title="Edit Project"
+                description={`Update information about project ${project.contractId}`}
+            />
+            <UpdateProjectForm
+                projectId={id}
+                action={updateProject}
+                defaultValues={project}
+            />
+        </div>
+    );
 }
