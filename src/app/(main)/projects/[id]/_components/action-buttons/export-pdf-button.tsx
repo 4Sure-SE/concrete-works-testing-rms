7371f191c64ad36fac6bec13aee0fc5b196f4176
ashtonregalado <167854841+ashtonregalado@users.noreply.github.com>
2025-04-29 18:31:53 +0800

    feat: implement Status of Test report generation (#31)

--- src/app/(main)/projects/[id]/_components/action-buttons/export-pdf-button.tsx
+++ src/app/(main)/projects/[id]/_components/action-buttons/export-pdf-button.tsx
@@ -8,6 +8,7 @@ import {
 import type { Projects } from "@/lib/types/project";
 import { BarChart2, ClipboardCheck, FileText } from "lucide-react";
 import downloadQCP from "../export-pdf/qcp-report";
+import downloadSOT from "../export-pdf/sot-report";
 
 export function ExportPdfButton({ project }: { project: Projects }) {
     return (
@@ -32,7 +33,7 @@ export function ExportPdfButton({ project }: { project: Projects }) {
                     Quality Control Program
                 </DropdownMenuItem>
                 <DropdownMenuItem
-                    onClick={() => console.log("Exporting Status of Test")}
+                    onClick={() => downloadSOT(project)}
                     className="flex cursor-pointer items-center rounded-sm px-3 py-2 text-sm text-gray-700 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none"
                 >
                     <BarChart2 className="mr-2 h-4 w-4 text-blue-500" />
