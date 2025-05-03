494f1acfa6e6f2cb1e5d3313ad689e9c13a094a7
Jose <joseearronn@gmail.com>
2025-05-03 12:59:10 +0800

    build: add storybook test addon

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,8 @@
 import "@/styles/globals.css";
 import type { Preview } from "@storybook/react";
+// Import the mock here for global application
+import { action } from "@storybook/addon-actions"; // Import action
+import {} from "@storybook/experimental-nextjs-vite/navigation.mock";
 
 const preview: Preview = {
     parameters: {
@@ -8,8 +11,23 @@ const preview: Preview = {
                 color: /(background|color)$/i,
                 date: /Date$/i,
             },
+            nextjs: {
+                appDirectory: true,
+            },
+        },
+        nextjs: {
+            appDirectory: true,
+            navigation: {
+                push: action("nextNavigation.push"),
+                replace: action("nextNavigation.replace"),
+                forward: action("nextNavigation.forward"),
+                back: action("nextNavigation.back"),
+                prefetch: action("nextNavigation.prefetch"),
+                refresh: action("nextNavigation.refresh"),
+            },
         },
     },
+    tags: ["autodocs"],
 };
 
 export default preview;
