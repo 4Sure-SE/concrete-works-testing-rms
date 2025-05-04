df77ba467d5d8e56c2ee237009193adbbcd3a55b
okjose <joseearronn@gmail.com>
2025-05-04 22:56:48 +0800

    feat: implement delete project (#45)

--- src/app/(main)/projects/_components/project-list/project-list.tsx
+++ src/app/(main)/projects/_components/project-list/project-list.tsx
@@ -1,7 +1,16 @@
 "use client";
 
-import type { ProjectSummaryDTO } from "@/lib/types/project";
+import type {
+    ProjectActionErrors,
+    ProjectDTO,
+    ProjectSummaryDTO,
+} from "@/lib/types/project";
 
+import type { Callbacks } from "@/lib/types/actions.types";
+import { withCallbacks } from "@/lib/utils";
+import { deleteProject } from "@/server/actions/projects/delete-project";
+import { useOptimistic, useState, useTransition } from "react";
+import { toast } from "sonner";
 import { ProjectItem } from "./project-item";
 import { ProjectListHeader } from "./project-list-header";
 
@@ -9,16 +18,64 @@ interface ProjectListProps {
     data: ProjectSummaryDTO[];
 }
 
+type OptimisticAction = { type: "DELETE"; payload: { id: string } };
+
 export function ProjectList({ data }: ProjectListProps) {
+    // delete callbacks
+    const callbacks: Callbacks<ProjectDTO | null, ProjectActionErrors> = {
+        onSuccess: () => {
+            toast.success("Project deleted successfully.");
+        },
+        onError: (error) => {
+            toast.error(error.general?.[0] ?? "Failed to delete project.");
+        },
+    };
+
+    // optimistic update for project list
+    const [projects, setProjects] = useOptimistic(
+        data,
+        (currentState, action: OptimisticAction) => {
+            switch (action.type) {
+                case "DELETE":
+                    return currentState.filter(
+                        (project) => project.id !== action.payload.id,
+                    );
+                default:
+                    return currentState;
+            }
+        },
+    );
+
+    const [, startTransition] = useTransition();
+    const [deletingId, setDeletingId] = useState<string | null>(null);
+
+    const deleteAction = async (id: string) => {
+        setDeletingId(id);
+        const deleteProjectAction = withCallbacks(
+            deleteProject.bind(null, id),
+            callbacks,
+        );
+        const res = await deleteProjectAction();
+
+        startTransition(async () => {
+            if (res.success && res.data) {
+                setProjects({ type: "DELETE", payload: { id: res.data.id } });
+                setDeletingId(null);
+            }
+        });
+    };
+
     return (
         <>
             <ProjectListHeader title="Projects" />
-            {data.length > 0 ? (
+            {projects.length > 0 ? (
                 <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
-                    {data.map((project) => (
+                    {projects.map((project) => (
                         <ProjectItem
                             key={project.id}
-                            {...project}
+                            data={project}
+                            onDeleteAction={deleteAction}
+                            disabled={deletingId === project.id}
                         />
                     ))}
                 </div>
