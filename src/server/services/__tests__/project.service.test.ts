1037253986ecac0e406bf54a120243011498b8d1
Sean Ashton V. Regalado <seanashton.regalado-23@cpu.edu.ph>
2025-05-10 22:52:27 +0800

    test: add test for getProjectDetails Project Service

--- src/server/services/__tests__/project.service.test.ts
+++ src/server/services/__tests__/project.service.test.ts
@@ -1,4 +1,5 @@
 import * as projectAdapter from "@/lib/adapters/project";
+import { fakeProjectWorkItemData } from "@/lib/stubs/project-details.stub";
 import { fakeProject, fakeProjectDTO } from "@/lib/stubs/project.stub";
 import * as projectDataAccess from "@/server/data-access/project/project";
 import { ProjectService } from "@/server/services/project.service";
@@ -68,6 +69,63 @@ describe("ProjectService", () => {
                 ProjectService.deleteProject(projectIdToDelete),
             ).rejects.toThrow(
                 `[Service] Failed to convert deleted project ID: ${projectIdToDelete}`,
+            );
+        });
+    });
+
+    describe("getProjectDetails", () => {
+        const projectId = fakeProject.id;
+
+        beforeEach(async () => {
+            // before each test
+            // clear all mocks and clear the projects and all associated dat
+            vi.clearAllMocks();
+            await projectDataAccess.clearProjects();
+        });
+
+        afterEach(async () => {
+            vi.restoreAllMocks();
+            await projectDataAccess.clearProjects();
+        });
+
+        it("successfully get ptoject details and return its DTO", async () => {
+            await projectDataAccess.createProject(fakeProject);
+            await ProjectService.createProjectWorkItem(
+                projectId,
+                fakeProjectWorkItemData,
+            );
+
+            const result = await ProjectService.getProjectDetails(projectId);
+
+            expect(result).toMatchObject({
+                contractId: fakeProject.contractId,
+                projectWorkItem: [
+                    expect.objectContaining({
+                        description: "Reinforced Concrete",
+
+                        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+                        itemTest: expect.arrayContaining([
+                            expect.objectContaining({
+                                balance: 1,
+                                requiredTests: 1,
+                                testQuantity: 1,
+                                testRequired: "Trial Mix",
+                                testsOnFile: 0,
+                            }),
+                        ]),
+                    }),
+                ],
+            });
+        });
+
+        it("should return an error if project is not found", async () => {
+            const fakeProjectId = "550e8400-e29b-41d4-a716-446655440000"; // valid UUID
+
+            // Use async/await with expect.rejects to check for error thrown
+            await expect(
+                ProjectService.getProjectDetails(fakeProjectId),
+            ).rejects.toThrowError(
+                `[Service] Project with ID ${fakeProjectId} not found`,
             );
         });
     });
